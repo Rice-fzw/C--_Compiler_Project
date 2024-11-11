4b6eba2f9160e826bcc7beeecb5b607e2f0a7f60
asnly <asnly1013@gmail.com>
2024-11-11 13:19:09 +0800

    done with 1.3

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <cassert>
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <string>
+#include "AST.h"
+
+using namespace std;
+
+extern FILE *yyin;
+extern int yyparse(unique_ptr<BaseAST> &ast);
+
+int main(int argc, const char *argv[]) {
+  assert(argc == 5);
+  auto mode = argv[1];
+  auto input = argv[2];
+  auto output = argv[4];
+
+  yyin = fopen(input, "r");
+  assert(yyin);
+
+  unique_ptr<BaseAST> ast;
+  auto ret = yyparse(ast);
+  assert(!ret);
+
+  ast->Dump();
+  cout<<endl;
+
+  return 0;
+}
