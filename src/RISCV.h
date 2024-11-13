07b23025cf484de9970c5cb49a791b1652b163f8
zwmy <123090825@link.cuhk.edu.cn>
2024-11-13 10:30:19 +0800

    test git bash

--- src/RISCV.h
+++ src/RISCV.h
@@ -1,1 +1,3 @@
-#include
+#include <iostream>
+#include "koopa.h"
+//docker run -it --rm -v .:/root/compiler maxxing/compiler-dev autotest -riscv -s lv1 /root/compiler
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <memory>
 #include <string>
 #include "AST.h"
+#include "RISCV.h"
 
 using namespace std;
 
