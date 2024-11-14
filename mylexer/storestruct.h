39bd69ff2778045f458b9f6fb4620fad75a908aa
jianghanyun <123090226@link.cuhk.edu.cn>
2024-11-15 00:26:50 +0800

    add my code

---
+++ mylexer/storestruct.h
@@ -0,0 +1,21 @@
+#ifndef s_h
+#define s_h
+
+#include <string>
+#include <vector>
+
+
+class Tokens{
+    public:
+        //constructor
+        Tokens();
+
+    private:
+        std::string value;
+        std::string type;  
+        int sum;  
+
+};
+
+
+#endif;
